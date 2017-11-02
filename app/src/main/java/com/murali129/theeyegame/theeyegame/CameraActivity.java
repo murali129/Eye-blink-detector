1f0bb0ba8f7173d66b91fc2bb5f4a2edf03b4356
murali krishna <kurapati.muralikrish@gmail.com>
2017-11-02 12:56:26 -0400

    first commit

---
+++ app/src/main/java/com/murali129/theeyegame/theeyegame/CameraActivity.java
@@ -0,0 +1,13 @@
+package com.murali129.theeyegame.theeyegame;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class CameraActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_camera);
+    }
+}
